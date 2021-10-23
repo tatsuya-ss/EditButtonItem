887f3c7085904f4eae02c75d41b53c8c593a6d04
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-10-23 21:17:14 +0900

    UIMenu表示後、編集を押した時だけキャンセルボタンに返す処理

--- EditButtonItem/ViewController.swift
+++ EditButtonItem/ViewController.swift
@@ -8,27 +8,48 @@
 import UIKit
 
 final class ViewController: UIViewController {
+    
+    private var editBarButton: UIBarButtonItem {
+        UIBarButtonItem(title: "編集", menu: makeEditMenu())
+    }
+    private var cancelBarButton: UIBarButtonItem {
+        UIBarButtonItem(title: "キャンセル",
+                        style: .plain,
+                        target: self,
+                        action: #selector(cancelBarButtonDidTapped))
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupEditButtonItem()
     }
-
-    override func setEditing(_ editing: Bool,
-                             animated: Bool) {
-        super.setEditing(editing, animated: animated)
-        switch editing {
-        case true:
-            navigationItem.rightBarButtonItem?.title = "キャンセル"
-        case false:
-            navigationItem.rightBarButtonItem?.title = "編集"
-        }
-    }
     
     // MARK: setupEditButtonItem
     private func setupEditButtonItem() {
-        navigationItem.rightBarButtonItem = editButtonItem
-        navigationItem.rightBarButtonItem?.title = "編集"
+        navigationItem.rightBarButtonItem = editBarButton
+    }
+    
+    private func makeEditMenu() -> UIMenu {
+        let addTimerAction = UIAction(title: "タイマーを作成",
+                                      state: .off) { _ in
+        }
+        let editTimerAction = UIAction(title: "タイマーを編集",
+                                       state: .off) { [weak self] _ in
+            self?.navigationItem.title = "タイマーを編集"
+            self?.navigationItem.rightBarButtonItem = self?.cancelBarButton
+        }
+        let deleteTimerAction = UIAction(title: "タイマーを削除",
+                                       state: .off) {  _ in
+            print("削除")
+        }
+        let editMenu = UIMenu(children: [addTimerAction,
+                                         editTimerAction,
+                                         deleteTimerAction])
+        return editMenu
+    }
+
+    @objc private func cancelBarButtonDidTapped() {
+        navigationItem.rightBarButtonItem = editBarButton
     }
     
 }
